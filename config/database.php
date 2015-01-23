d6656a48175ac486768d5731fda64428da6383ed
Scott Hummel <shummel@speareducation.com>
2015-01-23 16:18:44 -0700

    Trying again

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
 
 		'mysql' => [
 			'driver'    => 'mysql',
-			'host'      => env('DB_HOST', 'localhost'),
+			'host'      => getenv('DB_HOST'),
 			'database'  => env('DB_DATABASE', 'forge'),
 			'username'  => env('DB_USERNAME', 'forge'),
 			'password'  => env('DB_PASSWORD', ''),
