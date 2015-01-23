bbb5b94fe7b72752126689882ba732cdfc0b7133
Scott Hummel <shummel@speareducation.com>
2015-01-23 15:10:00 -0700

    Fixed getenv

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
 			'host'      => env('DB_HOST', 'localhost'),
 			'database'  => env('DB_DATABASE', 'forge'),
 			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => getenv('DB_PASSWORD', ''),
+			'password'  => getenv('DB_PASSWORD'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
