3a40eea6fd293e4ae6a1afb8525243d64e78bcf6
Scott Hummel <shummel@speareducation.com>
2015-02-13 10:48:16 -0700

    Added Facebook and Twitter links

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -6,7 +6,7 @@
   <img src="{{ URL::asset('images/larazona.png') }}" />
   <div class="title">Larazona</div>
   <p>You have arrived at Larazona's website, which is coming soon.</p>
-  <p>For more information, please visit <a href="http://www.meetup.com/larazona">Meetup.com</a></p>
-  <a href="mailto:larazonagroup@gmail.com">Email us</a>
+  <p>Larazona is a Scottsdale, AZ-based Laravel meetup. For more information, please visit <a href="http://www.meetup.com/larazona">Meetup.com</a></p>
+  <a href="mailto:larazonagroup@gmail.com">Email us</a> | <a href="https://www.facebook.com/groups/1427521640814284/">Facebook</a> | <a href="http://www.twitter.com/larazonagroup">Twitter</a>
 @stop
 
