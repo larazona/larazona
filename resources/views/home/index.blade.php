191be9871e29e97a97277c45a75f2ef65063124d
Scott Hummel <shummel@speareducation.com>
2015-07-24 11:38:10 -0700

    Created home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,10 +3,62 @@
 @section('title', 'Larazona :: Scottsdale, AZ Laravel Meetup')
 
 @section('content')
-  <img src="{{ URL::asset('images/larazona.png') }}" />
-  <div class="title">Larazona</div>
-  <p>You have arrived at Larazona's website, which is coming soon.</p>
-  <p>Larazona is a Scottsdale, AZ-based Laravel meetup. For more information, please visit <a href="http://www.meetup.com/larazona">Meetup.com</a></p>
-  <a href="mailto:larazonagroup@gmail.com">Email us</a> | <a href="https://www.facebook.com/groups/1427521640814284/">Facebook</a> | <a href="http://www.twitter.com/larazonagroup">Twitter</a>
+  <div class="row">
+    <div class="col-sm-4">
+        <a href="http://www.meetup.com/larazona"><img class="img-responsive" src="{{ URL::asset('images/larazona.png') }}" alt="Larazona logo"/></a>
+    </div>
+
+    <div class="col-sm-8">
+        <div class="title">Larazona</div>
+
+        <hr />
+
+        <p class="lead">"You have arrived" at Larazona's website.</p>
+        <p>Larazona is a Scottsdale, AZ-based Laravel meetup.<br /> For more information, please visit <a href="http://www.meetup.com/larazona">Meetup.com</a></p>
+
+        <p>Sign up for the meetup group, as that's how we stay in touch <br />with our members and announce each month's topic.</p>
+    </div>
+  </div>
+
+  <div class="row" style="text-align: left">
+      <div class="col-sm-4">
+          <div class="well">
+              <h3>Contact Information</h3>
+
+              <p><a href="mailto:larazonagroup@gmail.com">Email us</a> | <a href="https://www.facebook.com/groups/1427521640814284/">Facebook</a> | <a href="http://www.twitter.com/larazonagroup">Twitter</a></p>
+
+              <p><strong>Address:</strong> 7201 E. Princess Blvd, Scottsdale, AZ</p>
+
+              <hr />
+
+              <h4>Organizer</h4>
+
+              <p>You can reach Larazona organizer Scott Hummel at <a href="mailto:scott@scotthummel.net">this email address</a>. We'll be happy to post job listings or other items of interest to our group. Please no emails about website SEO or expanding the reach of the website.  This website is for our members.</p>
+          </div>
+
+          <h3>Next Month's Topic</h3>
+
+          <p>We'll be continuing our discussion of <strong>Redis</strong>.  We'll be looking at an in-depth case study of Redis usage, as well as how pub-sub works in Redis.  Then in September, we'll be starting The Project, where you can get hands-on coding experience.</p>
+      </div>
+
+      <div class="col-sm-8">
+          <h2 style="text-align: center;">About Larazona</h2>
+
+          <div class="row">
+              <div class="col-sm-6">
+                  <p>Larazona is a meetup based in Scottsdale, Arizona.  We meet monthly to discuss topics of interest to a Laravel developer.  Anyone with an interest in Laravel is welcome.  If you're not sold on Laravel, you're still welcome to come check us out and see if Laravel might be a framework solution for you.  If you're not using a framework, you're still welcome.  Our goal is to reach any current or potential Laravel developer.</p>
+                  <p>We're only a few months old &mdash; we formed in January 2015 &mdash; but already we've been offering a diverse array of topics for our developers.  We've had several topics on Laravel itself, including an introduction to the framework, Elixir, and how to mix Laravel with Angular.  We also had an excellent presentation on how to use Doctrine with Laravel (instead of Eloquent) and then how to test the code.  Then we had a couple of sessions on Redis.</p>
+              </div>
+              <div class="col-sm-6">
+                  <p>Next on our agenda is a collaborative project with our entire group, where we code a ticketing resale site, using the same API as Orbitz and Cheap Tickets use.  We'll be working out of a Github repo and making collaborative decisions on how best to approach the API, and what we can do it it.  Coding the project will take several sessions.  If all goes well, we may take it as far as production.  So come get some actually Laravel coding experience.</p>
+
+                  <p>In short, Larazona is a dynamic and growing group of developers.  We've got 70-some developers signed up through Meetup.com and we're growing nearly every day.  We have a core group of developers as well as an ever-changing array of newcomers.  So come meet some great developers, learn some best practices and stay for the pizza.  Yes, there's usually food.  We meet in Scottsdale, at the fabuluous Spear Education facility at Scottsdale Road and Princess Boulevard.</p>
+              </div>
+          </div>
+
+      </div>
+  </div>
+
+
 @stop
 
